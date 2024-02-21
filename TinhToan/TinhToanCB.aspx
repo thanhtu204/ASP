181cbd1ee8b740f65c9a8f781627f076b48d7c78
Windows 10 <Windows 10@DESKTOP-16O9K1Q>
2024-02-21 21:58:43 +0700

    Add project files.

---
+++ TinhToan/TinhToanCB.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TinhToanCB.aspx.cs" Inherits="TinhToan.TinhToanCB" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+      <link href="css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="text-center">
+           <h1 class="text-lowercase" >Bài Tập Đơn Gĩan</h1>
+             <div class="" style="margin-top: 117px">
+                       Nhập Số 1:<asp:TextBox ID="txt1" runat="server" style="width: 128px"></asp:TextBox>  
+                 </br>
+                 Nhập Số 2:<asp:TextBox ID="txt2" runat="server" style="width: 128px"></asp:TextBox> 
+                   </br>
+                 
+                 <asp:Button CssClass="btn btn-info " ID="btnnhap" runat="server" Text="Trừ" OnClick="btnnhap_Click"  />
+                 
+                 <asp:Button CssClass="btn btn-info  ml-2" ID="Button1" runat="server" Text="Nhân" OnClick="Button1_Click"  />
+                 
+                 <asp:Button CssClass="btn btn-info  ml-2" ID="Button2" runat="server" Text="Chia" OnClick="Button2_Click"  />
+                 
+                 <asp:Button CssClass="btn btn-info  ml-2" ID="Button3" runat="server" Text="Cộng" OnClick="Button3_Click" />
+                    
+            </div>
+            Kết quả: <asp:Label ID="lblhien" runat="server" Text=""></asp:Label>
+        </div>
+        
+    </form>
+    
+</body>
+</html>
+
---
+++ TinhToan/TinhToanCB.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TinhToan
+{
+    public partial class TinhToanCB : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnnhap_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 1";
+                return;
+            }
+            if (txt2.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 2";
+                return;
+            }
+            double mot = double.Parse(txt1.Text);
+            double hai = double.Parse(txt2.Text);
+
+
+
+
+            double tru = mot - hai;
+            lblhien.Text = mot + "-" + hai + "=" + tru;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 1";
+                return;
+            }
+            if (txt2.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 2";
+                return;
+            }
+            double mot = double.Parse(txt1.Text);
+            double hai = double.Parse(txt2.Text);
+
+
+
+
+            double tru = mot * hai;
+            lblhien.Text = mot + "*" + hai + "=" + tru;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 1";
+                return;
+            }
+            if (txt2.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 2";
+                return;
+            }
+            double mot = double.Parse(txt1.Text);
+            double hai = double.Parse(txt2.Text);
+
+
+
+
+            double tru = mot / hai;
+            lblhien.Text = mot + "/" + hai + "=" + tru;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 1";
+                return;
+            }
+            if (txt2.Text == "")
+            {
+                lblhien.Text = "bạn chưa nhập số 2";
+                return;
+            }
+            double mot = double.Parse(txt1.Text);
+            double hai = double.Parse(txt2.Text);
+
+
+
+
+            double tru = mot + hai;
+            lblhien.Text = mot + "+" + hai + "=" + tru;
+        }
+    }
+}
